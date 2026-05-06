1e9d4778105ecb3f61b8e6eae42a129f5651535a
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2026-05-06 16:19:51 +0500

    feat: add DashboardExecutiveKpis and DashboardOperationalAnalytics components

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { default as DashboardCompliancePosture } from './DashboardCompliancePosture/DashboardCompliancePosture';
+export { default as DashboardExecutiveKpis } from './DashboardExecutiveKpis/DashboardExecutiveKpis';
+export { default as DashboardOperationalAnalytics } from './DashboardOperationalAnalytics/DashboardOperationalAnalytics';
+export { default as DashboardTopItems } from './DashboardTopItems/DashboardTopItems';
